bd5a4664be3a011b00618373e7ad447c05ea6501
Carlos Sarmiento <c.sarmientobaca@gmail.com>
2022-12-09 14:25:18 +0100

    finished exercises js

--- D5.js
+++ D5.js
@@ -35,6 +35,8 @@ pets.push(pets.shift())
 
 console.log(pets)
 
+
+
 console.log('******ESERCIZIO 5*****')
 /* ESERCIZIO 5
     Dato il seguente array di oggetti, scrivi del codice per aggiungere ad ognuno di essi una proprietà 'licensePlate' con valore a tua scelta.
@@ -76,8 +78,9 @@ Object.assign(cars[2], plateVolk)
 console.log(cars)
 
 
-console.log('******ESERCIZIO 6*****')
 
+
+console.log('******ESERCIZIO 6*****')
 /* ESERCIZIO 6
     Scrivi del codice per aggiungere un nuovo oggetto in ultima posizione nell'array 'cars', rispettando la struttura degli altri elementi.
     Successivamente, rimuovi l'ultimo elemento della proprietà 'trims' da ogni auto.
@@ -123,6 +126,14 @@ console.log('******ESERCIZIO 8*****')
     'color' ha valore 'b', mostra in console 'Fizz'. Altrimenti, mostra in console 'Buzz'.
 */
 
+for (let i = 0; i < cars.length; i++) {
+  if (cars[i].color.charAt(0) === "b")
+    console.log("Fizz")
+  else {
+    console.log("Buzz")
+
+  }
+}
 
 
 
@@ -134,6 +145,16 @@ console.log('******ESERCIZIO 9*****')
 const numericArray = [
   6, 90, 45, 75, 84, 98, 35, 74, 31, 2, 8, 23, 100, 32, 66, 313, 321, 105,
 ]
+
+i = 0
+while (numericArray[i] !== 32) {
+  console.log(numericArray[i])
+  i++
+
+
+}
+
+
 console.log('******ESERCIZIO 10*****')
 /* ESERCIZIO 10
     Partendo dall'array fornito e utilizzando un costrutto switch, genera un nuovo array composto dalle posizioni di ogni carattere all'interno
@@ -141,3 +162,119 @@ console.log('******ESERCIZIO 10*****')
     es. [f, b, e] --> [6, 2, 5]
 */
 const charactersArray = ['g', 'n', 'u', 'z', 'd']
+
+const charactersPositions = []
+for (let i = 0; i < charactersArray.length; i++) {
+  switch (charactersArray[i]) {
+    case "a":
+      console.log("ok its a")
+      charactersPositions.push(1)
+      break;
+    case "b":
+      console.log("ok its b")
+      charactersPositions.push(2)
+
+      break;
+    case "c":
+      console.log("ok its c")
+      charactersPositions.push(3)
+
+      break;
+    case "d":
+      console.log("ok its d")
+      charactersPositions.push(4)
+
+      break;
+    case "e":
+      console.log("ok its e")
+      charactersPositions.push(5)
+
+      break;
+    case "f":
+      console.log("ok its f")
+      charactersPositions.push(6)
+
+      break;
+    case "g":
+      console.log("ok its g")
+      charactersPositions.push(7)
+
+      break;
+    case "h":
+      console.log("ok its h")
+      charactersPositions.push(8)
+
+      break;
+    case "i":
+      console.log("ok its i")
+      charactersPositions.push(9)
+
+      break;
+    case "l":
+      console.log("ok its l")
+      charactersPositions.push(10)
+
+      break;
+    case "m":
+      console.log("ok its m")
+      charactersPositions.push(11)
+
+      break;
+    case "n":
+      console.log("ok its n")
+      charactersPositions.push(12)
+
+      break;
+    case "o":
+      console.log("ok its o")
+      charactersPositions.push(13)
+
+      break;
+    case "p":
+      console.log("ok its p")
+      charactersPositions.push(14)
+
+      break;
+    case "q":
+      console.log("ok its q")
+      charactersPositions.push(15)
+
+      break;
+    case "r":
+      console.log("ok its r")
+      charactersPositions.push(16)
+
+      break;
+    case "s":
+      console.log("ok its s")
+      charactersPositions.push(17)
+
+      break;
+    case "t":
+      console.log("ok its t")
+      charactersPositions.push(18)
+
+      break;
+    case "u":
+      console.log("ok its u")
+      charactersPositions.push(19)
+
+      break;
+    case "v":
+      console.log("ok its v")
+      charactersPositions.push(20)
+
+      break;
+    case "z":
+      console.log("ok its z")
+      charactersPositions.push(21)
+
+      break;
+
+    default:
+      console.log("nope")
+      break;
+  }
+}
+
+console.log(charactersPositions)
